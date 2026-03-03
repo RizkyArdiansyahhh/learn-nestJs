ba9c732f3c6c2558e82d25990c0bc69948d35093
RizkyArdiansyahhh <rizky.ardiansyahhh06@gmail.com>
2026-03-03 15:33:46 +0700

    learn HTTP request

--- src/user/user/user.controller.ts
+++ src/user/user/user.controller.ts
@@ -1,7 +1,21 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Controller, Get, Param, Post, Query } from '@nestjs/common';
+import { Request } from 'express';
 
 @Controller('/api/users')
 export class UserController {
+  @Get('/hello')
+  sayHello(
+    @Query('first_name') firtsName: string,
+    @Query('last_name') lastName: string,
+  ) {
+    return `Hello ${firtsName} ${lastName}`;
+  }
+
+  @Get('/:id')
+  getById(@Param('id') id: string) {
+    return `Get ${id}`;
+  }
+
   @Post()
   post(): string {
     return 'POST';
